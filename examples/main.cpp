0e4dac619af081b71a38c1c219f58fe7c54df4b4
Daehan Jang <abouttt57@gmail.com>
2026-04-27 16:40:52 +0900

    Add array container implementation

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,28 @@
-#include "raw/dummy.h"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+#include "raw/array.h"
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	raw::array<int, 3> a1{ {1, 2, 3} };
+	raw::array<int, 3> a2 = { 1, 2, 3 };
+
+	std::sort(a1.begin(), a1.end());
+	std::ranges::reverse_copy(a2, std::ostream_iterator<int>(std::cout, " "));
+	std::cout << '\n';
+
+	raw::array<std::string, 2> a3{ "E", "\u018E" };
+	for (const auto& s : a3)
+	{
+		std::cout << s << ' ';
+	}
+	std::cout << '\n';
+
+	[[maybe_unused]] raw::array a4{ 3.0, 1.0, 4.0 };
+	[[maybe_unused]] raw::array<int, 2> a5;
+	[[maybe_unused]] raw::array<int, 2> a6{};
+	[[maybe_unused]] raw::array<int, 2> a7{ 1 };
 }
