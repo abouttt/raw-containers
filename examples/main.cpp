64b7eb56c1574172bea75829bf53abbad4dd80b7
Daehan Jang <abouttt57@gmail.com>
2026-05-13 20:01:25 +0900

    Add initial project scaffolding with CMake, examples and tests

---
+++ examples/main.cpp
@@ -0,0 +1,7 @@
+#include "raw/dummy.h"
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
---
+++ tests/dummy.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(DummyTest, Placeholder)
+{
+	EXPECT_TRUE(true);
+}
